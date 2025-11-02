11d12278aa41662220277e74de2deaabd54bb669
joelans <baileyderek59@gmail.com>
2025-11-02 18:00:13 +0300

    feat: add goodbye message

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
+import readline from 'readline';
+import { EOL } from 'os';
 import { getUsername } from './modules/getUsername.js';
+
 const username = getUsername();
 
-console.log(`Welcome to the File Manager, ${username}!`);
+process.stdout.write(`Welcome to the File Manager, ${username}!${EOL}`);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', (command) => {
+  if (command === '.exit') {
+    exit();
+  }
+});
+
+rl.on('SIGINT', () => {
+  exit();
+});
+
+const exit = () => {
+  process.stdout.write(`Thank you for using File Manager, ${username}, goodbye!${EOL}`);
+  rl.close();
+  process.exit(0);
+};
