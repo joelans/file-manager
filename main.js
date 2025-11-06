9f00ce334657b0908aad9006b68984bd8a9c0576
joelans <baileyderek59@gmail.com>
2025-11-06 22:06:26 +0300

    refactor(cd): move stdout writes from main to separate module

--- main.js
+++ main.js
@@ -25,32 +25,19 @@ prompt();
 rl.on('line', async (command) => {
   process.stdout.write(EOL);
   command = command.trim();
+
   if (command === '.exit') {
     exit();
   } if (command.startsWith('up')) {
     currentDir = up(command, currentDir);
   } else if (command.startsWith('cd')) {
-    const targetPath = command.slice(3);
-
-    if (targetPath === '') {
-      process.stdout.write(`Missing mandatory directory path.${EOL}`);
-    } else if (command[2] !== ' ') {
-      process.stdout.write(`Invalid command${EOL}`);
-    } else {
-      const result = await cd(currentDir, targetPath);
-      
-      if (result.success) {
-        currentDir = result.data;
-        process.stdout.write(result.message);
-      } else {
-        process.stdout.write(result.message);
-      }
-    }
+    currentDir = await cd(command, currentDir);
   } else if (command.startsWith('ls')) {
     await ls(command, currentDir);
   } else {
     process.stdout.write(`Invalid command${EOL}`);
   }
+  
   prompt();
 });
 
--- modules/cd.js
+++ modules/cd.js
@@ -1,32 +1,38 @@
 import { resolve, isAbsolute } from 'node:path';
-import { EOL } from 'os';
+import { EOL } from 'node:os';
 import { access } from 'node:fs/promises';
 
-const cd = async (currentDir, targetPath) => {
+const cd = async (command, currentDir) => {
+  if (command === 'cd') {
+    process.stdout.write(`Missing mandatory directory path${EOL}`);
+    return currentDir;
+  } else if (command[2] !== ' ') {
+    process.stdout.write(`Invalid command${EOL}`);
+    return currentDir;
+  }
+  const targetPath = command.slice(3);
+
   try {
     const path = isAbsolute(targetPath) ? resolve(targetPath) : resolve(currentDir, targetPath);
 
     await access(path);
 
-    return {
-      success: true,
-      message: `Directory is changed${EOL}`,
-      data: path
-    };
+    if (resolve(currentDir) === path ) {
+      const message = targetPath === '..' ? `Already at the root directory. Cannot go up${EOL}` : `Already in '${currentDir}' directory${EOL}`;
+      process.stdout.write(message);
+      return currentDir;
+    }
+
+    process.stdout.write(`Directory is changed${EOL}`);
+    return path;
+
   } catch (error) {
     if (error.code === 'ENOENT') {
-      return {
-        success: false,
-        message: `Path does not exist${EOL}`,
-        data: currentDir
-      };
+      process.stdout.write(`Path does not exist${EOL}`);
     } else {
-      return {
-        success: false,
-        message: `Cannot change directory: ${error.message}${EOL}`,
-        data: currentDir
-      };
+      process.stdout.write(`Cannot change directory: ${error.message}${EOL}`);
     }
+    return currentDir;
   }
 };
 
